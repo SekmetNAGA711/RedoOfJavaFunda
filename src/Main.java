4f2f0250626ea5aa384e315b5631d0737b362ff0
AbbyANG <bena.ao310@gmail.com>
2024-07-09 06:10:09 -0400

    redo fundamentals

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main { //This is the name of the recipe---- reminder -everything must go inside a class.
+
+    //Ingredients would go here
+
+
+
+    public static void main(String[] args) { //This is the method, methods always have brackets (), the name 'main' method is required. It is a starting point
+
+        System.out.println("Hello world!");
+        //Instructions for the method goes here
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
