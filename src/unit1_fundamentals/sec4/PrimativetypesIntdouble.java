74c1d317a5b66e204174bb035658fa8db592ef46
AbbyANG <bena.ao310@gmail.com>
2024-09-09 06:59:17 -0400

    added primative data types

---
+++ src/unit1_fundamentals/sec4/PrimativetypesIntdouble.java
@@ -0,0 +1,15 @@
+package unit1_fundamentals.sec4;
+
+public class PrimativetypesIntdouble {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
+}
--- src/unit1_fundamentals/sec4/Variables.java
+++ src/unit1_fundamentals/sec4/Variables.java
@@ -6,10 +6,15 @@ public class Variables {
     //violin = 2 - same for this
     //budgerigar = 8 - same for this
     //violin * budgerigar = 16 -
+    // + - * / ()
+    // We always work out the right side first and then the assignment happens
 
     public static void main(String[] args) {
-        int flute = 8;
-        System.out.println(flute);
+        int flute = 8 / 4 - 2; // order of operations
+        System.out.println(flute + 6);
+
+        int guitar = 8 / (4-2); //Order of operations
+        System.out.println(guitar + 6);
 
 
 
