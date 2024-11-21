4b152f3b4120e9001957d9ec50f1f259fde0e69c
AbbyANG <bena.ao310@gmail.com>
2024-11-21 09:36:35 -0500

    updated

--- src/unit1_fundamentals/sec4/VariableManipulationandtypes.java
+++ src/unit1_fundamentals/sec4/VariableManipulationandtypes.java
@@ -8,14 +8,30 @@ public class VariableManipulationandtypes {
         x = x + y + x;
         System.out.println(x);
 
+        double I = 8.8 + 8.8;
+        System.out.println(I);
+
+        boleanandchar();
+
 
     }
 
-    public static void boleanandchar () {
+
+    public static void boleanandchar() {
         boolean c = false;
         char t = 'z';// char is based on unicode, example - the number the 65 is capital A, 66 is B, etc till 90 which is z
 // char can hold symbols, numbers and other languages.
         char e = 169; //This will give me the copywrite symbol
+        char z = '\u005A';
+        String A = "Christmas Holiday";//Strings are reference data types
+
+        System.out.println(c);
+        System.out.println(t);
+        System.out.println(e);
+        System.out.println(z);
+        System.out.println(A);
+        System.out.println("Abso" + "lutely"); // this is called string concatenation
+
 
     }
 
