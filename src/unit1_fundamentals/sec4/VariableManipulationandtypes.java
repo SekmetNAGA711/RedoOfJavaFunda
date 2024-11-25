12362d824ba203b0945021470518a591c8cdc3ca
AbbyANG <bena.ao310@gmail.com>
2024-11-25 10:16:17 -0500

    updated

--- src/unit1_fundamentals/sec4/VariableManipulationandtypes.java
+++ src/unit1_fundamentals/sec4/VariableManipulationandtypes.java
@@ -11,6 +11,8 @@ public class VariableManipulationandtypes {
         double I = 8.8 + 8.8;
         System.out.println(I);
 
+        double statueHeightInMetres = 14.27;//Good practice to use Camelcase
+
         boleanandchar();
 
 
@@ -18,19 +20,22 @@ public class VariableManipulationandtypes {
 
 
     public static void boleanandchar() {
-        boolean c = false;
+        boolean hasDriversLicense = false;
         char t = 'z';// char is based on unicode, example - the number the 65 is capital A, 66 is B, etc till 90 which is z
 // char can hold symbols, numbers and other languages.
         char e = 169; //This will give me the copywrite symbol
         char z = '\u005A';
         String A = "Christmas Holiday";//Strings are reference data types
 
-        System.out.println(c);
+        System.out.println(hasDriversLicense);
         System.out.println(t);
         System.out.println(e);
         System.out.println(z);
         System.out.println(A);
         System.out.println("Abso" + "lutely"); // this is called string concatenation
+
+        hasDriversLicense = true;  //has yourself a statement and whether it is true or false, good practice
+        System.out.println(hasDriversLicense);
 
 
     }
