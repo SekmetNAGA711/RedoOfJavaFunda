64766dcbd908e0aefe5b7b4ac244883a81ff2cf7
AbbyANG <bena.ao310@gmail.com>
2024-12-24 12:09:44 -0500

    updated

---
+++ src/unit1_fundamentals/sec5/UserInput.java
@@ -0,0 +1,17 @@
+package unit1_fundamentals.sec5;
+import java.util.Scanner;
+
+public class UserInput {
+
+
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);// scanner needs to be declared just like variables
+
+     String input1 = scanner.next(); //now I can use the scanner
+
+        System.out.println(input1);
+
+    }
+
+
+}
