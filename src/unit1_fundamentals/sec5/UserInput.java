f364c4c2b74af20d4ecbcc50cc4bf64880ceb912
AbbyANG <bena.ao310@gmail.com>
2024-12-25 07:14:40 -0500

    updated

--- src/unit1_fundamentals/sec5/UserInput.java
+++ src/unit1_fundamentals/sec5/UserInput.java
@@ -1,4 +1,5 @@
 package unit1_fundamentals.sec5;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class UserInput {
@@ -9,7 +10,24 @@ public class UserInput {
 
      String input1 = scanner.next(); //now I can use the scanner
 
-        System.out.println(input1);
+        System.out.println("\n" + input1);
+
+        AskQuestion();
+
+
+    }
+
+    public static void AskQuestion(){
+        System.out.println("What is your name?");
+        Scanner scanner2 = new Scanner(System.in);//Create a new scanner, tell it to read from the keyboard (System.in)
+        String UserName = scanner2.next(); //next.() is used to scan strings, store it in a variable called UserInput,
+        // create a string variable for the next() method to pick it up
+        System.out.println(UserName + " is a wonderful name ");
+
+        System.out.println("\nHow old are you?");
+        int UserAge = scanner2.nextInt();// asking the scanner to scan for an integer
+        System.out.println("Your name is " + UserName + " and you are " + UserAge + " years old");
+
 
     }
 
