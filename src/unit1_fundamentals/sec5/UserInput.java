c14de0a45b7ac336ef286dae0de9d0507f0af88f
AbbyANG <bena.ao310@gmail.com>
2024-12-26 09:38:36 -0500

    updated

--- src/unit1_fundamentals/sec5/UserInput.java
+++ src/unit1_fundamentals/sec5/UserInput.java
@@ -18,16 +18,19 @@ public class UserInput {
     }
 
     public static void AskQuestion(){
-        System.out.println("What is your name?");
         Scanner scanner2 = new Scanner(System.in);//Create a new scanner, tell it to read from the keyboard (System.in)
+        System.out.println("What is your name?");
         String UserName = scanner2.next(); //next.() is used to scan strings, store it in a variable called UserInput,
         // create a string variable for the next() method to pick it up
         System.out.println(UserName + " is a wonderful name ");
 
-        System.out.println("\nHow old are you?");
+        System.out.println("\nHow old are you, " + UserName + "?");
         int UserAge = scanner2.nextInt();// asking the scanner to scan for an integer
         System.out.println("Your name is " + UserName + " and you are " + UserAge + " years old");
 
+        System.out.println("\nHow tall are you in metres?");
+        double UserHeight = scanner2.nextDouble();
+        System.out.println("You are " + UserHeight + " metres tall.");
 
     }
 
