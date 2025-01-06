52e165d89db211ab0e266987e903e29ae0f187d1
AbbyANG <bena.ao310@gmail.com>
2025-01-06 07:19:57 -0500

    updated

--- src/unit1_fundamentals/sec6/ConditionalStatements.java
+++ src/unit1_fundamentals/sec6/ConditionalStatements.java
@@ -4,6 +4,22 @@ public class ConditionalStatements {
 
     public static void main(String[] args) {
 
+        /*operators (number left bigger than number on the right - false*Test
+        > greater than
+        >= greater than or equal to
+        <less than
+        <= less than or equal to
+
+        Equality operators
+        == equal to
+        != not equal to
+        */
+
+if(true) {} //when the condition in the brackets is true the program in the curly brackets ie the body will run if not it will not
+
+
+
+
     }
 
 
