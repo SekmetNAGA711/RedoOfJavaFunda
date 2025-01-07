5f242c942d1c0a9ac8d495a356e64404605951ae
AbbyANG <bena.ao310@gmail.com>
2025-01-07 08:05:06 -0500

    updated

--- src/unit1_fundamentals/sec6/ConditionalStatements.java
+++ src/unit1_fundamentals/sec6/ConditionalStatements.java
@@ -1,5 +1,8 @@
 package unit1_fundamentals.sec6;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class ConditionalStatements {
 
     public static void main(String[] args) {
@@ -15,7 +18,28 @@ public class ConditionalStatements {
         != not equal to
         */
 
-if(true) {} //when the condition in the brackets is true the program in the curly brackets ie the body will run if not it will not
+
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to SuperFast roller coaster");
+        System.out.print("Please enter your heigh in centimetres: ");
+        int height = scanner.nextInt();
+
+//when the condition in the brackets is true the program in the curly brackets ie the body will run if not it will not
+if(height<130) {
+
+    System.out.println("Sorry, you are too short.");
+}
+else if(height > 220){//once an 'if' is apart add a () and {}//
+    System.out.println("Sorry, you are too tall");
+}
+else {
+    System.out.println("Height accepted. Go ahead.");
+}
+//else if blocks must go in between, if statements must come first.
+
+
+
+
 
 
 
