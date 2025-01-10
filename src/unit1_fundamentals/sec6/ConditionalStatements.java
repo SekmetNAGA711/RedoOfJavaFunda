87f70f3c0ef779de781107ac1ce9d03bdbafea58
AbbyANG <bena.ao310@gmail.com>
2025-01-10 10:53:17 -0500

    updated

--- src/unit1_fundamentals/sec6/ConditionalStatements.java
+++ src/unit1_fundamentals/sec6/ConditionalStatements.java
@@ -25,20 +25,21 @@ Scanner scanner = new Scanner(System.in);
         int height = scanner.nextInt();
 
 //when the condition in the brackets is true the program in the curly brackets ie the body will run if not it will not
-if(height >130 || height < 220) { // the || is the 'or' operator, only one expression has to be true for the block to run
-    //&& logic operator, both have to be true for the statement to run
+if(height >130 || height < 220) { /* the || is the 'or' operator, only one expression has to be true for the block to run
+    && logic operator, both have to be true for the statement to run*/
 
     System.out.println("Height accepted. Go ahead.");
 }
-//else if(height > 220){//once an 'if' is apart add a () and {}//
-//    System.out.println("Sorry, you ");
-//}
+/* else if(height > 220) {-----once an 'if' is apart add a () and {}
+   System.out.println("Sorry, you ");
+} */
 else {
     System.out.println("Sorry, you do not meet the height requirements");
 }
 //else if blocks must go in between, if statements must come first.
 
         comparingStrings();
+        lexicalScope();
 
 
     }
@@ -61,6 +62,33 @@ else {
 
 
     }
+
+
+
+    public static void lexicalScope() {
+        /*Can not declare a variable in an if statement and use it outside the if statement. Variables declared
+        inside the body of an if statement are called local variables, meaning can only be used inside the if
+        statement. If a variable is declared within a method, it is localized within the that method and can be used only in
+        that method and an if statement should there be an if statement in that method/* */
+
+        int numPets = -1; /* if we declared it outside the if statement, it must be initialized with a value because should the if statement turn out to be false
+                            then the value numPets in the if statement below will throw and error, because the then the numPet will never be initialized because
+                            it is false, that means it can not be printed, Also sometimes it is better to give an invalid value like -1 because the user might enter
+                            0 /**/
+        if(true){
+       Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter the number of pets you own: ");
+         numPets= scanner.nextInt(); /*this variable would only be available within this if statement, if it was declared within this if statement, so we declared it outside the if statement,
+                                     it would have been declared like this - int numPets = scanner.nextInt()/* */
+
+     }
+        System.out.println("You have " + numPets + " pets");
+
+
+    }
+
+
+
 
 
 
