21f465191d3d95c2b51137b3df11f189beca508d
AbbyANG <bena.ao310@gmail.com>
2025-01-14 05:56:39 -0500

    updated

--- src/unit1_fundamentals/sec6/ConditionalStatements.java
+++ src/unit1_fundamentals/sec6/ConditionalStatements.java
@@ -87,7 +87,8 @@ else {
 
     }
 
-    public static void switchStatement() {
+    public static void switchStatement() { /*The switch statement is like a specialized if statement.
+                                          It can be useful when you want to compare a single variable against a number of values./**/
 
 
 
