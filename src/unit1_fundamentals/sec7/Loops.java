882ccf30af8dac74bfaf6e062d731f17afda2558
AbbyANG <bena.ao310@gmail.com>
2025-01-22 08:17:23 -0500

    updated

---
+++ src/unit1_fundamentals/sec7/Loops.java
@@ -0,0 +1,28 @@
+package unit1_fundamentals.sec7;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        //While loop.
+        /* With an if statement when the end of the body is reached, the program will simply continue. However, when the
+        end of the body is reached on a while loop, the program will go back up to the top and check the condition again.
+        If it's true, it will run the body again and it will keep repeating this until the condition is false. At which point
+         it will stop looping and simply skip the body and continue. So whereas an if statement will only run once a while,
+          loop can run many times.
+         */
+        while(false ){
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
