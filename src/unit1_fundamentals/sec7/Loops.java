69aae9bd4450899ffe6051dd9dd99d090b50c081
AbbyANG <bena.ao310@gmail.com>
2025-01-23 07:00:36 -0500

    updated

--- src/unit1_fundamentals/sec7/Loops.java
+++ src/unit1_fundamentals/sec7/Loops.java
@@ -9,9 +9,14 @@ public class Loops {
         If it's true, it will run the body again and it will keep repeating this until the condition is false. At which point
          it will stop looping and simply skip the body and continue. So whereas an if statement will only run once a while,
           loop can run many times.
+          The condition is true at first, but then turns false at some point in the future. A common way is to create a variable
+          which acts like a counter.
          */
-        while(false ){
 
+        int i = 0;
+        while(5 < 10 ){
+            System.out.println("test");
+            i = i +1;
 
         }
 
