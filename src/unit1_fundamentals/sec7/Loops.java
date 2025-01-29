2eb53c8f13d9569e4e1d6318782a65e256a7fb3d
AbbyANG <bena.ao310@gmail.com>
2025-01-29 07:36:04 -0500

    updated

--- src/unit1_fundamentals/sec7/Loops.java
+++ src/unit1_fundamentals/sec7/Loops.java
@@ -10,22 +10,48 @@ public class Loops {
          it will stop looping and simply skip the body and continue. So whereas an if statement will only run once a while,
           loop can run many times.
           The condition is true at first, but then turns false at some point in the future. A common way is to create a variable
-          which acts like a counter.
+          which acts like a counter. note: the variable can be used anywhere else
          */
 
         int i = 0;
-        while(5 < 10 ){
+        while(i < 10 ){
             System.out.println("test1");
-            i = i +1;
+            i = i +1; //this can be written like this i++, i +=1 for increase or i --
 
+        }
+
+        forLoop();
+
+
+
+    }
+
+
+    public static void forLoop(){
+        /* The ForLoop is the compact version of achieving the while loop of increasing and decreasing
+         * In the for loop the parentheses () hold the variable, the condition and the incrementation.
+         * note: the variable is only available to the  for look  */
+
+        for (int i = 0;  i <10; i++) {
+            System.out.println("Test");
+
+        }
+
+        //
+
+        for(int i = 22;  i > 10; i--){
+            System.out.println(i);
         }
 
 
 
 
 
-
     }
+
+
+
+
 
 
 
