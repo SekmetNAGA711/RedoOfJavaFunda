c0566f3ee289a8e6675c93b8b27d34b0167362ac
AbbyANG <bena.ao310@gmail.com>
2025-06-17 05:56:51 -0400

    updated

--- src/unit1_fundamentals/Sec8/AdaptiveIteration.java
+++ src/unit1_fundamentals/Sec8/AdaptiveIteration.java
@@ -2,14 +2,16 @@ package unit1_fundamentals.Sec8;
 
 public class AdaptiveIteration {
     public static void main(String[] args) {//using length is a great way to be adaptive. No matter how many elements are in the array, we will print it
-        int[] nums = {8, 88, 88, 8, 26};
+        int[] nums = {8, 88, 80, 8, 26};
 
-        for( int i = 0; i < nums.length; i++) {
+        for( int i = 0; i < nums.length; i++) {//note i< nums.length is saying as long as the index is less than the amount of numbers in the array.,
+            // starting with index 0 and so on, the last index would be index 4, so it will only go to index 4.
             System.out.println(nums[i]);
         }
 
 
-        for(int i = nums.length -1; i >=0; i--) {  //This code will print it backwards, we have to take -1 from length because of the positioning of indexes
+        for(int i = nums.length -1; i >=0; i--) {  //This code will print it backwards, we have to take -1 from length because of the positioning of indexes, meaning we are starting at the last index,
+            // sometimes we may not know what the last index is, so we write it i = nums.length - 1 (the -1 because indexes are always 1 less than actual count)
             System.out.println(nums[i]); // sidenote -in the for loop the first item is the starting point (where do you want to start)
         }
 
